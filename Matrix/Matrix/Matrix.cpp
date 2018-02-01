73539ad8a1abfc39881d215a35f7ceeb81029c2c
Jae Jang <jaejang5566@gmail.com>
2018-01-31 22:14:50 -0800

    set and get value implementation

--- Matrix/Matrix/Matrix.cpp
+++ Matrix/Matrix/Matrix.cpp
@@ -24,6 +24,18 @@ Matrix::~Matrix() {
 }
 
 
+//Put the value into the specific index
+//PRE	: All parameters need to be integers
+//PRE	: The index must not be out of bound
+//POST	: the value is assigned to the index
+void Matrix::set_value(const int row, const int col, const int value) {
+	array[row * size + col] = value;
+}
 
-
+//Get value for the index
+//PRE	: The index must not be out of bound
+//RETURN: an integer for the index
+int Matrix::get_value(const int row, const int col) {
+	return array[row*size + col];
+}
 
